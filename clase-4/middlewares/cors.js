aba5b51970d420707c8b5a7307239b47dc63c375
TevenV27 <victoria.kevin@correounivalle.edu.co>
2023-08-20 04:28:35 -0500

    MVC

--- clase-4/middlewares/cors.js
+++ clase-4/middlewares/cors.js
@@ -1,22 +1,22 @@
 import cors from 'cors'
 
 const ACCEPTED_ORIGINS = [
-  'http://localhost:8080',
-  'http://localhost:1234',
-  'https://movies.com',
-  'https://midu.dev'
-]
+    'http://localhost:8080',
+    'http://localhost:1234',
+    'https://movies.com',
+    'https://midu.dev'
+  ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
-  origin: (origin, callback) => {
-    if (acceptedOrigins.includes(origin)) {
-      return callback(null, true)
-    }
 
-    if (!origin) {
-      return callback(null, true)
+    origin: (origin, callback) => {
+      if (ACCEPTED_ORIGINS.includes(origin)) {
+        return callback(null, true)
+      }
+      if (!origin) {
+        return callback(null, true)
+      }
+      return callback(new Error('Not allowed by CORS'))
     }
-
-    return callback(new Error('Not allowed by CORS'))
-  }
-})
+    
+  })
