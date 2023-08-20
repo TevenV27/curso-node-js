1c96ba573412c3d3d533c087874f63e6f707f047
TevenV27 <victoria.kevin@correounivalle.edu.co>
2023-08-20 05:07:55 -0500

    Mongo DB and MVC

--- clase-4/models/local-file-system/movie.js
+++ clase-4/models/local-file-system/movie.js
@@ -1,7 +1,7 @@
 import { createRequire } from 'node:module'
 import { randomUUID } from 'node:crypto'
 const require = createRequire(import.meta.url)
-const movies = require('../movies.json')
+const movies = require('../../movies.json')
 
 export class MovieModel {
 
